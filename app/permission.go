5427dc18945bbd1f3416990c6c44ce2f51a914ac
Brooke Bryan <brooke@bajb.net>
2021-04-30 15:38:57 +0100

    Permissions

--- app/permission.go
+++ app/permission.go
@@ -7,3 +7,23 @@ type Permission struct {
 	Name        translation.Text
 	Description translation.Text
 }
+
+type PermissionEffect string
+
+const (
+	PermissionEffectAllow PermissionEffect = "Allow"
+	PermissionEffectDeny  PermissionEffect = "Deny"
+)
+
+type PermissionStatement struct {
+	Effect     PermissionEffect
+	Permission Permission
+	Resource   string // path or resource indicator defined by the app
+}
+
+type PermissionPolicy struct {
+	Key         string
+	Name        translation.Text
+	Description translation.Text
+	Statements  []PermissionStatement
+}
