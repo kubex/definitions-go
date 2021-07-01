eae4d8e0156adbf1dc94420bfa99cf54012d9489
Brooke Bryan <brooke@bajb.net>
2021-07-01 15:51:12 +0100

    Add vendor from json

--- app/vendor.go
+++ app/vendor.go
@@ -1,10 +1,22 @@
 package app
 
-import "github.com/kubex/definitions-go/translation"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/kubex/definitions-go/translation"
+)
 
 type Vendor struct {
 	ID                 string
 	Name               translation.Text
 	Description        translation.Text
 	AuthenticationData map[string]string
+}
+
+func VendorFromJson(jsonBytes []byte) (*Vendor, error) {
+	def := &Vendor{}
+	if err := json.Unmarshal(jsonBytes, def); err != nil {
+		return nil, errors.New("unable to decode vendor definition json")
+	}
+	return def, nil
 }
