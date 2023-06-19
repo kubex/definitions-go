72abb540c9665bc2c606cbb7c888b5f9cbb8bf43
Brooke Bryan <brooke@bajb.net>
2023-06-19 16:00:03 +0100

    Updated app definitions

--- app/definition.go
+++ app/definition.go
@@ -10,18 +10,19 @@ import (
 const VendorAppID = "kx" // setting your application ID to this value should be done for vendor shared
 
 type Definition struct {
-	ID              GlobalAppID      `json:"id"`
-	Name            translation.Text `json:"name"`
-	Description     translation.Text `json:"description,omitempty"`
-	Endpoint        string           `json:"endpoint,omitempty"`
-	UIMode          UIMode           `json:"UIMode,omitempty"`
-	Category        Category         `json:"category,omitempty"`
-	Icon            string           `json:"icon,omitempty"`         // Default icon to use for this application
-	Dependencies    []GlobalAppID    `json:"dependencies,omitempty"` // Other applications this app depends on
-	Permissions     []Permission     `json:"permissions,omitempty"`  // Permissions made available by this application
-	Paths           []Path           `json:"paths,omitempty"`
-	Unify           []Navigation     `json:"unify,omitempty"` // How to link with other applications
-	ActivationSteps []ActivationStep `json:"activationSteps,omitempty"`
+	ID              GlobalAppID        `json:"id"`
+	Name            translation.Text   `json:"name"`
+	Description     translation.Text   `json:"description,omitempty"`
+	Endpoint        string             `json:"endpoint,omitempty"`
+	UIMode          UIMode             `json:"UIMode,omitempty"`
+	Category        Category           `json:"category,omitempty"`
+	Icon            string             `json:"icon,omitempty"`         // Default icon to use for this application
+	Dependencies    []GlobalAppID      `json:"dependencies,omitempty"` // Other applications this app depends on
+	Permissions     []Permission       `json:"permissions,omitempty"`  // Permissions made available by this application
+	Navigation      []Navigation       `json:"navigation,omitempty"`   // Global app navigation
+	Paths           []Path             `json:"paths,omitempty"`
+	Unify           []IntegrationPoint `json:"unify,omitempty"` // How to link with other applications
+	ActivationSteps []ActivationStep   `json:"activationSteps,omitempty"`
 
 	Homepage       string `json:"homepage,omitempty"`       // https:// url
 	TermsOfService string `json:"termsOfService,omitempty"` // https:// url
--- app/navigation.go
+++ app/navigation.go
@@ -5,26 +5,27 @@ import "github.com/kubex/definitions-go/translation"
 type IntegrationLocation string
 
 const (
-	IntegrationLocationPageNav IntegrationLocation = "nav.page"
-	IntegrationLocationAppNav  IntegrationLocation = "nav.app"
-	IntegrationLocationAction  IntegrationLocation = "action"
-	IntegrationLocationPanel   IntegrationLocation = "panel"
+	IntegrationLocationPageNav IntegrationLocation = "nav.page" // Load within the page
+	IntegrationLocationAppNav  IntegrationLocation = "nav.app"  // Load as the primary app
+	IntegrationLocationAction  IntegrationLocation = "action"   // Perform an action, expecting a toast response
+	IntegrationLocationPanel   IntegrationLocation = "panel"    // Load within a panel
 )
 
 type LaunchMode string
 
 const (
-	LaunchModePage     LaunchMode = "page"     // (default) Render in the page
-	LaunchModeModal    LaunchMode = "modal"    // Launch in a modal
-	LaunchModeLightbox LaunchMode = "lightbox" // Launch in a modal
-	LaunchModeWindow   LaunchMode = "window"   // Launch in a new window
-	LaunchModeOverlay  LaunchMode = "overlay"  // Right side overlay app
+	LaunchModePage  LaunchMode = "page"  // (default) Render in the page
+	LaunchModeModal LaunchMode = "modal" // Launch in a modal
+	//LaunchModeLightbox LaunchMode = "lightbox" // Launch in a modal
+	LaunchModeWindow  LaunchMode = "window"  // Launch in a new window
+	LaunchModeOverlay LaunchMode = "overlay" // Right side overlay app
 )
 
 type IntegrationPoint struct {
-	IntegrateApp GlobalAppID         `json:"integrateApp,omitempty"`
-	Location     IntegrationLocation `json:"location,omitempty"`
-	PathID       string              `json:"pathID,omitempty"`
+	IntegrateApp GlobalAppID         `json:"integrateApp,omitempty"` // what app to load into
+	Location     IntegrationLocation `json:"location,omitempty"`     // Where to load the app
+	PathID       string              `json:"pathID,omitempty"`       //remote app path ID
+	Navigation   Navigation          `json:"navigation,omitempty"`   // Navigation, when panel location, dst path is used
 }
 
 type Navigation struct {
@@ -32,9 +33,7 @@ type Navigation struct {
 	Text            translation.Text `json:"text,omitempty"`
 	Title           translation.Text `json:"title,omitempty"`
 	DestinationPath string           `json:"destinationPath,omitempty"`
-	SectionID       string           `json:"sectionID,omitempty"`
 	LaunchMode      LaunchMode       `json:"launchMode,omitempty"`
-	Point           IntegrationPoint `json:"point,omitempty"`
 }
 
 type NavigationSection struct {
