811b75fee33b830d46af0bfd4ea9a57ab065b328
Brooke Bryan <brooke@bajb.net>
2023-09-22 10:19:51 +0100

    Add additional generic categories

--- app/categories.go
+++ app/categories.go
@@ -1,45 +1,37 @@
 package app
 
+import "strings"
+
 type Category string
 
 const (
-	CategoryGeneral   Category = "general"
-	CategoryProduct   Category = "product"
-	CategoryLeads     Category = "leads"
-	CategorySales     Category = "sales"
-	CategoryBilling   Category = "billing"
-	CategoryDelivery  Category = "delivery"
-	CategoryRetain    Category = "retain"
-	CategoryRecover   Category = "recover"
-	CategoryUtilities Category = "utilities"
-	CategorySettings  Category = "settings"
-	CategorySecurity  Category = "security"
+	CategoryAdministration Category = "administration"
+	CategoryMarketing      Category = "marketing"
+	CategoryDevelopment    Category = "development"
+	CategorySupport        Category = "support"
+	CategoryPayments       Category = "payments"
+	CategoryCommunication  Category = "communication"
+	CategoryChurn          Category = "churn"
+	CategoryRenewal        Category = "renewal"
+	CategoryFinance        Category = "finance"
+	CategoryKnowledge      Category = "knowledge"
+	CategoryOperations     Category = "operations"
+	CategoryReporting      Category = "reporting"
+	CategoryCustomers      Category = "customers"
+	CategoryGeneral        Category = "general"
+	CategoryProduct        Category = "product"
+	CategoryLeads          Category = "leads"
+	CategorySales          Category = "sales"
+	CategoryBilling        Category = "billing"
+	CategoryDelivery       Category = "delivery"
+	CategoryRetain         Category = "retain"
+	CategoryRecover        Category = "recover"
+	CategoryUtilities      Category = "utilities"
+	CategorySettings       Category = "settings"
+	CategorySecurity       Category = "security"
+	CategoryHR             Category = "human-resources"
 )
 
 func (c Category) Name() string {
-	switch c {
-	case CategoryGeneral:
-		return "General"
-	case CategoryProduct:
-		return "Product"
-	case CategoryLeads:
-		return "Leads"
-	case CategorySales:
-		return "Sales"
-	case CategoryBilling:
-		return "Billing"
-	case CategoryDelivery:
-		return "Delivery"
-	case CategoryRetain:
-		return "Retain"
-	case CategoryRecover:
-		return "Recover"
-	case CategoryUtilities:
-		return "Utilities"
-	case CategorySettings:
-		return "Settings"
-	case CategorySecurity:
-		return "Security"
-	}
-	return "Uncategorized"
+	return strings.ToTitle(string(c))
 }
