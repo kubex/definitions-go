ffaade83f2174dac354653c28e2f0d7ff34ee016
Brooke Bryan <brooke@bajb.net>
2025-06-09 14:28:03 +0100

    Version data

--- app/paths.go
+++ app/paths.go
@@ -3,5 +3,6 @@ package app
 const PathHealthCheck = "/_kubex/health"
 const PathDefinition = "/_kubex/definition"
 const PathAbout = "/_kubex/about"
+const PathVersion = "/_kubex/version"
 
 const PathPrefixWebhooks = "/_webhooks/"
