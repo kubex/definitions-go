f3c6942ea428acdfbbc8e00b9524bc336d0a72a7
Brooke Bryan <brooke@bajb.net>
2025-06-09 14:32:29 +0100

    Update to build time

--- app/version.go
+++ app/version.go
@@ -3,7 +3,7 @@ package app
 type Version struct {
 	Environment string `json:"environment"` // Environment name (e.g., "production", "sandbox")
 	Version     string `json:"version"`     // Application version
-	BuildDate   string `json:"buildDate"`   // Build date in RFC3339 format
+	BuildTime   string `json:"buildTime"`   // Build date in RFC3339 format
 	BuildID     string `json:"buildID"`     // Build ID
 	Commit      string `json:"commit"`      // Commit hash
 }
