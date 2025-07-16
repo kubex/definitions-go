2e91d14b11c17348dabc81af168fd5337e47f86c
Alan Jones <jonezzyboy@gmail.com>
2025-07-16 16:26:11 +0100

    Add optional `CountPath` for expanding action `EntryPoint` (#2)

--- app/entry.go
+++ app/entry.go
@@ -55,6 +55,7 @@ type EntryPoint struct {
 	Text                translation.Text `json:"text,omitempty"`
 	Title               translation.Text `json:"title,omitempty"`
 	DestinationPath     string           `json:"destinationPath,omitempty"`
+	CountPath           string           `json:"countPath,omitempty"` // Count Path - expanding actions only
 	LaunchMode          LaunchMode       `json:"launchMode,omitempty"`
 	RequiredPermissions []ScopedKey      `json:"requiredPermissions,omitempty"` // Permissions that must be set for the user to see this item
 }
