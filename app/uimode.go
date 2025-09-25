4523c67680f7aa31d5f3e234b9051b9c18d4d164
Brooke Bryan <brooke@bajb.net>
2025-09-25 16:02:43 +0100

    Add help ui

--- app/uimode.go
+++ app/uimode.go
@@ -7,5 +7,6 @@ const (
 	UIModeDextral     UIMode = "dextral"     // Dextral application
 	UIModeBasic       UIMode = "basic"       // simple application - used for non-native applications
 	UIModeIntegration UIMode = "integration" // Integrated into existing pages
+	UIModeHelp        UIMode = "help"        // Help App
 	UIModeNone        UIMode = "none"        // No UI - flow only
 )
