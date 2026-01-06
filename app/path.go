c36b6ad9121daa5edf24e2a5881fee70450195cc
Brooke Bryan <brooke@bajb.net>
2026-01-06 13:35:20 +0000

    Request Configuration to be passed on a path

--- app/path.go
+++ app/path.go
@@ -27,6 +27,7 @@ type Path struct {
 	RequiredPermissions []ScopedKey `json:"requiredPermissions,omitempty"` // Permissions that must be set for the user to call this page
 
 	BuiltInResources []BuiltInResource `json:"builtInResources,omitempty"`
+	RequestConfig    []ScopedKey       `json:"requestConfig,omitempty"` // Configuration values that should be sent to this path
 
 	Navigation []EntryPoint `json:"navigation,omitempty"`
 	Actions    []EntryPoint `json:"actions,omitempty"`
